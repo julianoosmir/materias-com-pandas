9a11715c39bfe840f73ed69420549b5fe57be566
juliano.hein <juliano.hein@azi.com.br>
2021-11-26 14:08:23 -0400

    lambada

--- materias.py
+++ materias.py
@@ -9,5 +9,8 @@ with open("materias.json", encoding='utf-8') as meu_json:
 materias = [n for n in lst if  n['situacao'] == "Aprovado"]
 df = pd.DataFrame(materias)
 
-df.to_csv("materias Aprovado.csv")
-for i in materias: print(i) 
+fil  = filter((lambda x: x['situacao'] == "Aprovado"),lst)
+for i in fil: print(i) 
+
+# df.to_csv("materias Aprovado.csv")
+# for i in materias: print(i) 
