5530c5cdf64a79b4c5d768df300eb0cfc4f9c359
julianoosmir <julianoosmir@outlook.com>
2021-11-26 19:14:14 -0400

    deleted

--- materias.py
+++ materias.py
@@ -1,16 +1,15 @@
 import json
 import pandas as pd
 
-
 with open("materias.json", encoding='utf-8') as meu_json:
     dados = json.load(meu_json)
     lst = list(dados)
-   
-materias = [n for n in lst if  n['situacao'] == "Aprovado"]
+
+materias = [n for n in lst if n['situacao'] == "Aprovado"]
 df = pd.DataFrame(materias)
 
-fil  = filter((lambda x: x['situacao'] == "Aprovado"),lst)
-for i in fil: print(i) 
+fil = filter((lambda x: x['situacao'] == "Aprovado"), lst)
+for i in fil: print(i)
 
 # df.to_csv("materias Aprovado.csv")
-# for i in materias: print(i) 
+# for i in materias: print(i)
